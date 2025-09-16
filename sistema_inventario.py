b7fee1ee771a549adaf94404c1f81df998d2ed75
Rodrigo <gallardor974@gmail.com>
2025-09-16 13:40:25 +0200

    Update sistema_inventario.py

--- sistema_inventario.py
+++ sistema_inventario.py
@@ -3,14 +3,17 @@ class Producto:
   def __init__(self,nombre,precio,cantidad):
     while True:
       try:
-        self.nombre = str(nombre)
-        break
+        if not nombre
+          print("Nombre inexistente")
+        else:
+          self.nombre = str(nombre)
+          break
       except (NameError, TypeError):
         print("Nombre inválido")
     while True:
       try:
-        if precio<=0:
-          print('El precio no puede ser negativo o nulo')
+        if precio<0:
+          print('El precio no puede ser negativo')
         else:
           self.precio = float(precio)
           break
@@ -18,8 +21,8 @@ class Producto:
         print("Precio inválido")
     while True:
       try:
-        if cantidad<=0:
-          print('La cantidad no puede ser negativa o nula')
+        if cantidad<0:
+          print('La cantidad no puede ser negativa')
         else:
           self.cantidad = int(cantidad)
           break
@@ -32,8 +35,8 @@ class Producto:
   def actualizar_precio(self, nuevo_precio):
     while True:
       try:
-        if nuevo_precio<=0:
-          print('El precio no puede ser negativo o nulo')
+        if nuevo_precio<0:
+          print('El precio no puede ser negativo')
         else:
           self.precio = nuevo_precio
           break
@@ -43,8 +46,8 @@ class Producto:
   def actualizar_cantidad(self, nueva_cantidad):
     while True:
       try:
-        if nueva_cantidad<=0:
-          print('La cantidad no puede ser negativa o nula')
+        if nueva_cantidad<0:
+          print('La cantidad no puede ser negativa')
         else:
           self.cantidad = nueva_cantidad
           break
@@ -60,14 +63,15 @@ class Inventario:
     self.lista_productos = []
 
   def agregar_producto(self, producto):
-    self.lista_productos.append(producto)
+    if isinstance(producto,Producto):
+      self.lista_productos.append(producto)
 
-  def buscar_producto(self, nombre):
-    nombre_productos = [producto.nombre for producto in self.lista_productos]
-    if nombre in nombre_productos:
-      return f"Producto encontrado"
-    else:
-      return f"Producto no encontrado"
+  def buscar_producto(self, producto):
+    if isinstance(producto,Producto):
+      if producto in self.lista_productos:
+        return f"Producto encontrado. Producto: {producto.nombre} - Precio: {producto.precio} € - Cantidad: {producto.cantidad} unidades"
+      else:
+        return f"Producto no encontrado"
 
   def calcular_valor_inventario(self):
     valor_productos = [producto.calcular_valor_total() for producto in self.lista_productos]
@@ -75,27 +79,27 @@ class Inventario:
     return valor_total
 
   def listar_productos(self):
-    lista = [{"Nombre": elemento.nombre, "Precio": elemento.precio, "Cantidad": elemento.cantidad} for elemento in self.lista_productos]
-    return f"{lista}"
+    for producto in self.lista_productos:
+        print(f"Producto: {producto.nombre} - Precio: {producto.precio} € - Cantidad: {producto.cantidad} unidades")
 
-def menu_principal(Inventario):
+def menu_principal(inventario,producto):
   print("¿Qué desea hacer? 1 Agregar producto 2 Buscar producto 3 Calcular valor 4 Listar productos")
-  decision = int(input("Seleccione el número asociado"))
   try:
+    decision = int(input("Seleccione el número asociado"))
     if (decision==1):
-      return Inventario.agregar_producto()
+      return inventario.agregar_producto(producto)
     elif (decision==2):
-      return Inventario.buscar_producto()
+      return inventario.buscar_producto(producto)
     elif (decision==3):
-      return Inventario.calcular_valor_inventario()
+      return inventario.calcular_valor_inventario()
     elif (decision==4):
-      return Inventario.listar_productos()
+      return inventario.listar_productos()
     else:
       return f"Entrada no válida"
   except (ValueError, TypeError):
     return f"Entrada no válida"
 
-if __name__ = "__main__":
+if __name__ == "__main__":
   mi_inventario = Inventario()
   menu_principal(mi_inventario)
 
